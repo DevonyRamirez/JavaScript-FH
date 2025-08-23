f2bc4c63003dee9dda3d6aa213f7991a4a918989
Devony <duodevo@gmail.com>
2025-08-23 16:37:34 -0300

    Funciones final

--- 01_Fundamentos/assets/js/funciones.js
+++ 01_Fundamentos/assets/js/funciones.js
@@ -1,8 +1,10 @@
 
 
 function saludar(nombre){
-    console.log( arguments ); // Muestra todos los argumentos que se pasan a la función
-    console.log('Hola '+ nombre);
+    // console.log( arguments ); // Muestra todos los argumentos que se pasan a la función
+    // console.log('Hola '+ nombre);
+    return [1,2,3,4];
+    console.log('Soy código que no se va a ejecutar');
 }
 
 const saludar2=function(nombre){
@@ -14,6 +16,27 @@ const saludarFlecha=(nombre)=>{
 }
 
 
-saludar('Devony',18,false,'Venezuela');
+const retornoDeSaludar=saludar('Devony',18,false,'Venezuela');
 // saludar2('Devony');
-saludarFlecha('Devony');
+console.log( retornoDeSaludar[0],retornoDeSaludar[1] );
+// saludarFlecha('Devony');
+
+
+function sumar(a,b){
+    return a+b;
+}
+
+const sumar2=(a,b)=> a+b; // return implícito
+
+
+
+function getAleatorio(){
+    return Math.random();
+}
+
+const getAleatorio2= ()=> Math.random();
+
+
+
+console.log( getAleatorio2() );
+console.log( sumar2(2,2) );
